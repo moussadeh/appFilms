fbd3166cd758b896f0a2685e2c49c55f7ab5fbb1
moussa <moussadeh92@gmail.com>
2024-09-28 14:27:12 +0200

    navbar avec icones

--- src/components/navigation/AppNavigator.tsx
+++ src/components/navigation/AppNavigator.tsx
@@ -5,7 +5,7 @@ import HomeScreen from "../../screens/HomeScreen";
 import SearchScreen from "../../screens/SearchScreen";
 import WishListScreen from "../../screens/WishListScreen";
 import ProfileScreen from "../../screens/ProfileScreen";
-
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,10 +13,10 @@ const AppNavigator = () => {
     return (
         <NavigationContainer>
             <Tab.Navigator screenOptions={{ tabBarActiveTintColor: 'yellow', tabBarInactiveTintColor: 'black', headerShown: false }}>
-                <Tab.Screen name="Home" component={HomeScreen}></Tab.Screen>
-                <Tab.Screen name="Search" component={SearchScreen}></Tab.Screen>
-                <Tab.Screen name="WishList" component={WishListScreen}></Tab.Screen>
-                <Tab.Screen name="Profile" component={ProfileScreen}></Tab.Screen>
+                <Tab.Screen name="Home" component={HomeScreen} options={{tabBarIcon: ({color, size}) => (<Icon name="home" color={color} size={size} />)}}></Tab.Screen>
+                <Tab.Screen name="Search" component={SearchScreen} options={{tabBarIcon: ({color, size}) => (<Icon name="search" color={color} size={size} />)}}></Tab.Screen>
+                <Tab.Screen name="WishList" component={WishListScreen} options={{tabBarIcon: ({color, size}) => (<Icon name="heart" color={color} size={size} />)}}></Tab.Screen>
+                <Tab.Screen name="Profile" component={ProfileScreen} options={{tabBarIcon: ({color, size}) => (<Icon name="person" color={color} size={size} />)}}></Tab.Screen>
             </Tab.Navigator>
         </NavigationContainer>
     )
